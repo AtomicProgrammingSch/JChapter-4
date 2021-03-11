a2b342daecc35b2043e65d7a45e4500a6fb3ba71
AtomicProgramming <georgetimms2005@outlook.com>
2021-03-11 12:05:59 +0000

    Finished task-3

---
+++ src/com/atomicprogramming/JChapter4/Task3.java
@@ -0,0 +1,27 @@
+package com.atomicprogramming.JChapter4;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+    private static final double pi = (double) 22/7;
+
+    public static double areaOfCircle(double radius) {
+        return pi * Math.pow(radius, 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the radius of the circle:");
+        double radius = 0;
+        if (scanner.hasNextDouble()) {
+            radius = scanner.nextDouble();
+        }
+        else {
+            System.out.println("You can not enter a string!");
+            System.exit(1);
+        }
+        double area = areaOfCircle(radius);
+        System.out.println("The area of the circle is: " + area);
+    }
+}
